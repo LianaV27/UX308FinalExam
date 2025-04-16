b0eb77b0866f998185b0e0b5df3f593e911f647b
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-16 23:37:52 +0000

    add colour mix function

--- src/functions.js
+++ src/functions.js
@@ -7,4 +7,55 @@ function fahrenheitToCelsius(fahrenheit){
     return celsius;
 }
 
-export {hello, fahrenheitToCelsius}
+
+/*
+        -------------------------------------------------------
+        Determines the secondary rgb_colour from mixing two primary
+        RGB (Red, Green, Blue) colours. The order of the colours
+        is *not* significant.
+        Returns "Error" if any of the rgb_colour parameter(s) are invalid.
+            "red" + "blue": "fuchsia"
+            "red" + "green": "yellow"
+            "green" + "blue": "aqua"
+            "red" + "red": "red"
+            "blue" + "blue": "blue"
+            "green" + "green": "green"
+        Use: rgb_colour = colour_mix(rgb_colour1, rgb_colour2)
+        -------------------------------------------------------
+        Parameters:
+            rgb_colour1 - a primary RGB rgb_colour (str)
+            rgb_colour2 - a primary RGB rgb_colour (str)
+        Returns:
+            rgb_colour - a secondary RGB rgb_colour (str)
+        -------------------------------------------------------
+    */
+
+function colour_mix(rgb_colour1, rgb_colour2){
+
+    let rgb_colour;
+
+    if (rgb_colour1 == "red" && rgb_colour2 == "blue"){
+        rgb_colour = "fuchsia";
+
+    } else if (rgb_colour1 == "red" && rgb_colour2 == "green"){
+        rgb_colour = "yellow";
+
+    } else if (rgb_colour1 == "green" && rgb_colour2 == "blue"){
+        rgb_colour = "aqua";
+
+    } else if (rgb_colour1 == "red" && rgb_colour2 == "red"){
+        rgb_colour = "red";
+
+    } else if (rgb_colour1 == "blue" && rgb_colour2 == "blue"){
+        rgb_colour = "blue";
+
+    } else if (rgb_colour1 == "green" && rgb_colour2 == "green"){
+        rgb_colour = "green";
+    
+    } else {
+        rgb_colour = "Error";
+    }
+    return rgb_colour;
+}
+
+export {hello, fahrenheitToCelsius, colour_mix}
