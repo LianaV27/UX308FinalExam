15563231d76c9584955dcfe14e9714dd24edd7db
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-17 02:00:52 +0000

    clean up code

--- src/functions.js
+++ src/functions.js
@@ -102,25 +102,6 @@ function pay_raise(status, years, salary) {
     return new_salary;
 }
 
-/*
-   -------------------------------------------------------
-   Determines if a year is a leap year. Every year that is
-   exactly divisible by four is a leap year, except for years
-   that are exactly divisible by 100, but these centurial years
-   are leap years if they are exactly divisible by 400. 
-   
-   For
-   example, the years 1700, 1800, and 1900 are not leap years,
-   but the years 1600 and 2000 are.
-   Use: result = is_leap(year)
-   -------------------------------------------------------
-   Parameters:
-       year - a year (int > 0)
-   Returns:
-       result - True if year is a leap year,
-           False otherwise (boolean)
-   ------------------------------------------------------
-   */
 
 function is_leap(year) {
 
