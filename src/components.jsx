29928a1a19e38cecbc1a7163cfc424fd4125cb60
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-04-17 01:16:02 +0000

    complete & test Q3

--- src/components.jsx
+++ src/components.jsx
@@ -1,10 +1,10 @@
-import {  colour_mix, largest_product } from './functions.js';
+import {  colour_mix, largest_product, day_of_the_week } from './functions.js';
 
 function Question1() {
     return <section>
         <h2>Question 1</h2>
         1. Determines the secondary rgb_colour from mixing two primary RGB colours.
-        <h2>results</h2>
+        <h3>results</h3>
         <p>colour_mix("blue", "red") == "{colour_mix("blue", "red")}"</p>
         <p>colour_mix("red", "green") == "{colour_mix("green", "red")}"</p>
         <p>colour_mix("blue", "green") == "{colour_mix("green", "blue")}"</p>
@@ -15,12 +15,23 @@ function Question2() {
     return <section>
         <h2>Question 2</h2>
         2. Returns the product of the two largest values of val1, val2, and val3.
-        <h2>results</h2>
+        <h3>results</h3>
         <p>largest_product(-4,10,18) == "{largest_product(-4,10,18)}"</p>
         <p>largest_product(5,4,3) == "{largest_product(5,4,3)}"</p>
         <p>largest_product(6,-5,7) == "{largest_product(6,-5,7)}"</p>
     </section>
 }
 
-export { Question1, Question2 }
+function Question3() {
+    return <section>
+        <h2>Question 3</h2>
+        2. Returns the name of a day of the week given an integer day number.
+        <h3>results</h3>
+        <p>day_of_the_week(1)) == "{day_of_the_week(1)}"</p>
+        <p>day_of_the_week(5)) == "{day_of_the_week(5)}"</p>
+        <p>day_of_the_week(7)) == "{day_of_the_week(7)}"</p>
+    </section>
+}
+
+export { Question1, Question2, Question3 }
 
--- src/functions.js
+++ src/functions.js
@@ -30,6 +30,7 @@ function colour_mix(rgb_colour1, rgb_colour2){
 
 
 function largest_product(val1, val2, val3){
+
     let product;
 
      // 1 & 2
@@ -47,5 +48,48 @@ function largest_product(val1, val2, val3){
        return product;
     }
 
+    /*
+        -------------------------------------------------------
+        Returns the name of a day of the week given an integer day number.
+        Day 1 is "Sunday", day 7 is "Saturday".
+        Returns "Error" if the number is not valid.
+        Use: day = day_of_the_week(day_num)
+        -------------------------------------------------------
+        Parameters:
+            day_num - day number (1 <= int <= 7)
+        Returns:
+            day - name of a day of the week (str)
+        ------------------------------------------------------
+        */
+    function day_of_the_week(day_num){
+        
+        let day;
 
-export {colour_mix, largest_product}
+        if (day_num == 1){
+            day = "Sunday";
+
+        } else if (day_num == 2){
+            day = "Monday";
+
+        } else if (day_num == 3){
+            day = "Tuesday";
+
+        } else if (day_num == 4){
+            day = "Wednesday";
+
+        } else if (day_num == 5){
+            day = "Thursday";
+
+        } else if (day_num == 6){
+            day = "Friday";
+
+        } else if(day_num == 7){
+            day = "Saturday";
+
+        } else{
+            day = "Error";
+        }
+        return day;
+    }
+
+export {colour_mix, largest_product, day_of_the_week}
